94b3369f95126d24f9d166089c6711a750744742
Radulf321 <63254742+Radulf321@users.noreply.github.com>
2025-05-20 08:55:08 +0200

    Style fix

--- Assets/Scripts/Cards/CardPile.cs
+++ Assets/Scripts/Cards/CardPile.cs
@@ -8,40 +8,50 @@ public class CardPile
     public List<Card> hand;
     public List<Card> discardPile;
     public List<Card> inPlay;
-    
-    public CardPile(List<Card> deck) {
+
+    public CardPile(List<Card> deck)
+    {
         this.deck = new List<Card>(deck);
         this.hand = new List<Card>();
         this.discardPile = new List<Card>();
         this.inPlay = new List<Card>();
     }
 
-    public void DrawCard(bool updateView = true) {
-        if (deck.Count <= 0) {
+    public void DrawCard(bool updateView = true)
+    {
+        if (deck.Count <= 0)
+        {
             ShuffleDiscardIntoDeck();
         }
 
-        if (deck.Count > 0) {
+        if (deck.Count > 0)
+        {
             Card drawnCard = deck[0];
             deck.RemoveAt(0);
             hand.Add(drawnCard);
         }
 
-        if (updateView) {
+        if (updateView)
+        {
             CombatHandler.instance.updateView();
         }
     }
 
-    public void DrawCards(int numberOfCards) {
-        for (int i = 0; i < numberOfCards; i++) {
+    public void DrawCards(int numberOfCards)
+    {
+        for (int i = 0; i < numberOfCards; i++)
+        {
             DrawCard(false);
         }
         CombatHandler.instance.updateView();
     }
 
-    public void DiscardRandomCards(int numberOfCards) {
-        for (int i = 0; i < numberOfCards; i++) {
-            if (hand.Count <= 0) {
+    public void DiscardRandomCards(int numberOfCards)
+    {
+        for (int i = 0; i < numberOfCards; i++)
+        {
+            if (hand.Count <= 0)
+            {
                 throw new Exception("No cards to discard.");
             }
             int index = UnityEngine.Random.Range(0, hand.Count);
@@ -57,7 +67,8 @@ public class CardPile
         ShuffleDeck();
     }
 
-    public List<Card> GetHand() {
+    public List<Card> GetHand()
+    {
         return this.hand;
     }
 
@@ -70,7 +81,8 @@ public class CardPile
     {
         System.Random rand = new System.Random();
         List<Card> newDeck = new List<Card>();
-        while (deck.Count > 0) {
+        while (deck.Count > 0)
+        {
             int index = rand.Next(deck.Count);
             newDeck.Add(deck[index]);
             deck.RemoveAt(index);
@@ -78,26 +90,33 @@ public class CardPile
         this.deck = newDeck;
     }
 
-    public void DiscardCard(Card card, bool updateView = true) {
-        if (hand.Contains(card) || inPlay.Contains(card)) {
+    public void DiscardCard(Card card, bool updateView = true)
+    {
+        if (hand.Contains(card) || inPlay.Contains(card))
+        {
             hand.Remove(card);
             inPlay.Remove(card);
             discardPile.Add(card);
-            if (updateView) {
+            if (updateView)
+            {
                 CombatHandler.instance.updateView();
             }
-        } else {
+        }
+        else
+        {
             throw new Exception("Card not in hand.");
         }
     }
 
-    public void AddCardToPlay(Card card) {
+    public void AddCardToPlay(Card card)
+    {
         hand.Remove(card);
         inPlay.Add(card);
         CombatHandler.instance.updateView();
     }
 
-    public void RemoveCard(Card card) {
+    public void RemoveCard(Card card)
+    {
         hand.Remove(card);
         inPlay.Remove(card);
         discardPile.Remove(card);
