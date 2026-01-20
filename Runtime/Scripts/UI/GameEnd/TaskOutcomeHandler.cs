d6f2db78bf5afbce48b4a158c5d626b21409e4e1
DrNiels <63254742+Radulf321@users.noreply.github.com>
2026-01-20 23:18:31 +0100

    Adjust height of TaskOutcome to required height for text

--- Runtime/Scripts/UI/GameEnd/TaskOutcomeHandler.cs
+++ Runtime/Scripts/UI/GameEnd/TaskOutcomeHandler.cs
@@ -7,7 +7,6 @@ public class TaskOutcomeHandler : MonoBehaviour
 {
     private GameTask? task;
     bool needUpdate = false;
-    float previousParentHeight = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,15 +14,15 @@ public class TaskOutcomeHandler : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    async void Update()
     {
         if (needUpdate)
         {
-            _ = UpdateDescription();
+            await UpdateDescription();
             Transform rewardArea = transform.Find("RewardDisplayArea");
             bool completed = task?.IsCompleted() ?? false;
             rewardArea.gameObject.SetActive(completed);
-            float preferredHeight = transform.Find("TaskDescription").GetComponent<LayoutElement>().preferredHeight;
+            float preferredHeight = transform.Find("TaskDescription").GetComponent<TMPro.TextMeshProUGUI>().preferredHeight;
             if (completed)
             {
                 rewardArea.GetComponent<RewardAreaHandler>().SetRewards(this.task!.GetRewards());
@@ -42,12 +41,7 @@ public class TaskOutcomeHandler : MonoBehaviour
 
     protected virtual void OnRectTransformDimensionsChange()
     {
-        float parentHeight = transform.parent.GetComponent<RectTransform>().rect.height;
-        if (parentHeight != previousParentHeight)
-        {
-            previousParentHeight = parentHeight;
-            needUpdate = true;
-        }
+        needUpdate = true;
     }
 
     private async Task UpdateDescription()
